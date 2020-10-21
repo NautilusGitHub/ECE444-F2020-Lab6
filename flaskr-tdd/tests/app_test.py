efbabe9990c3f52345c87cc3fb4af5ff146636b7
NautilusGitHub <jasonshang64@gmail.com>
2020-10-21 14:13:41 -0400

    Search feature added

--- flaskr-tdd/tests/app_test.py
+++ flaskr-tdd/tests/app_test.py
@@ -75,6 +75,19 @@ def test_messages(client):
     assert b"&lt;Hello&gt;" in rv.data
     assert b"<strong>HTML</strong> allowed here" in rv.data
 
+def test_search(client):
+    """Ensure that search works"""
+    login(client, app.config["USERNAME"], app.config["PASSWORD"])
+    rv = client.post(
+        "/add",
+        data=dict(title="<Hello>", text="<strong>HTML</strong> allowed here"),
+        follow_redirects=True,
+    )
+
+    rv = client.get('/search', query_string=dict(query="Hello"), follow_redirects=True)
+    print(rv.data)
+    assert b"Hello" in rv.data
+
 def test_delete_message(client):
     """Ensure the messages are being deleted"""
     rv = client.get('/delete/1')
